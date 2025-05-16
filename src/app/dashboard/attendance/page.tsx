1243671a9d0800ab5c527cb291814d6d6d7c2c43
Taufiq Hidayatulloh <hidayatullohtaufiq0@gmail.com>
2025-05-16 10:23:01 +0700

    feat(attendance): slicing attendance and detail page for admin

--- src/app/dashboard/attendance/page.tsx
+++ src/app/dashboard/attendance/page.tsx
@@ -1,11 +1,40 @@
-"use client"
 
-export default function AttendancePage() {
-    return (
-      <div className="rounded-lg border p-4">
-        <h2 className="text-lg font-medium mb-4">Attendance Management</h2>
-        <p>Attendance content here</p>
-      </div>
-    )
-  }
-  
+import { Metadata } from "next"
+import fs from "fs"
+import path from "path"
+import { columns } from "../../../components/table/attendance/admin/columns"
+import { DataTable } from "../../../components/table/attendance/admin/data-table"
+import { CustomPage, CustomPageHeader, CustomPageSubtitle, CustomPageTitle, CustomPageTitleButtons, CustomPageTitleContainer } from "@/components/ui/custom-page"
+
+export const metadata: Metadata = {
+  title: "Attendance",
+  description: "A Expense tracker build using Tanstack Table."
+};
+
+async function getData() {
+  const filePath = path.join(
+    process.cwd(),
+    "src/app/dashboard/attendance",
+    "data.json"
+  );
+  const data = fs.readFileSync(filePath, "utf8");
+  return JSON.parse(data);
+}
+
+export default async function Page() {
+  const data = await getData();
+  console.log("data", data);
+
+  return (
+    <CustomPage>
+      <CustomPageHeader>
+        <CustomPageTitleContainer>
+          <CustomPageTitle>Attendance Management</CustomPageTitle>
+          <CustomPageSubtitle>Manage and organize all your attendance' data</CustomPageSubtitle>
+        </CustomPageTitleContainer>
+      </CustomPageHeader>
+      <DataTable data={data} columns={columns} />
+      {/* <h2 className="text-lg font-medium mb-4">Employee Management</h2> */}
+    </CustomPage>
+  );
+}
---
+++ src/components/table/attendance/admin/data.tsx
@@ -0,0 +1,17 @@
+import { ArrowDownIcon, ArrowUpIcon } from "lucide-react";
+
+
+export const attendanceType = [
+  { value: "ontime", label: "On Time"},
+  { value: "late", label: "Late" },
+  { value: "sick leave", label: "Sick Leave" },
+  { value: "absent", label: "Absent" },
+  { value: "annual leave", label: "Annual Leave" },
+
+];
+
+export const approval = [
+  { value: "approve", label: "Approve"},
+  { value: "rejected", label: "Rejected"},
+  { value: "waiting", label: "Waiting"},
+];
