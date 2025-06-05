023806633c6c1701e51944c80bec385dbf2a5c51
Taufiq Hidayatulloh <hidayatullohtaufiq0@gmail.com>
2025-06-05 20:59:48 +0700

    refactor(deployment): refactor some lines for deployment

--- next.config.ts
+++ next.config.ts
@@ -4,6 +4,9 @@ const nextConfig: NextConfig = {
   images: {
     domains: ["randomuser.me"],
   },
+  eslint: {
+    ignoreDuringBuilds: true, // Ignore ESLint during builds (optional)
+  },
 };
 
 export default nextConfig;
