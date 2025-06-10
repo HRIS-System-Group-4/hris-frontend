949265d5f651a9f427bc271aac723610f7e89848
FajarBayuKusuma <114029998+FajarBayuKusuma@users.noreply.github.com>
2025-06-11 00:07:17 +0700

    perbaikan dashboardService

--- src/services/dashboardService.ts
+++ src/services/dashboardService.ts
@@ -1,15 +1,15 @@
 import axiosInstance from "@/lib/axios";
-import axios from "axios";
 
 export const getAdminDashboardStats = async () => {
   const token = localStorage.getItem("token");
   if (!token) throw new Error("Token not found");
 
-  const response = await axiosInstance.get("http://127.0.0.1:8000/api/index", {
+  const response = await axiosInstance.get("/api/index", {
     headers: {
       Authorization: `Bearer ${token}`,
       Accept: "application/json",
     },
+    withCredentials: true
   });
 
   return response.data;
