5201debb76a1935553ddf122f6b0a3d30f3441d8
Taufiq Hidayatulloh <hidayatullohtaufiq0@gmail.com>
2025-06-12 00:45:40 +0700

    fix(preprod) fix data type

--- src/lib/getAttendanceLocal.ts
+++ src/lib/getAttendanceLocal.ts
@@ -1,11 +1,10 @@
-import { AttendanceAdmin } from "@/components/table/attendance/admin/schema"
-import fs from "fs"
-import path from "path"
-
+import { AttendanceAdmin } from "@/components/table/attendance/admin/schema";
+import fs from "fs";
+import path from "path";
 
 export async function getLocalAttendanceById(id: string): Promise<AttendanceAdmin | null> {
-  const filePath = path.join(process.cwd(), "src/app/dashboard/attendance/data.json")
-  const rawData = fs.readFileSync(filePath, "utf-8")
-  const data: AttendanceAdmin[] = JSON.parse(rawData)
-  return data.find(item => item.id === Number(id)) ?? null
+  const filePath = path.join(process.cwd(), "src/app/dashboard/attendance/data.json");
+  const rawData = fs.readFileSync(filePath, "utf-8");
+  const data: AttendanceAdmin[] = JSON.parse(rawData);
+  return data.find(item => item.id === id) ?? null;
 }
